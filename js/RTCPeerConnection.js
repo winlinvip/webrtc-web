98b7d9646a4f15c81f2547a420eedaf1a265d456
winlin <winlin@vip.126.com>
2017-02-22 11:56:39 +0800

    Refine the peer connection tutorial

--- js/RTCPeerConnection.js
+++ js/RTCPeerConnection.js
@@ -7,57 +7,88 @@ navigator.webkitGetUserMedia({
     video:true,audio:false
 },function(stream){
     lv.src = window.URL.createObjectURL(stream);
-    console.log("local stream " + lv.src);
+    console.log("[navigator.webkitGetUserMedia] lv.src=localStream " + lv.src);
 
     rtcCall(stream);
 }, function(error){
     console.error(error);
 });
 
+function copyObject(obj) {
+    var cp = {};
+    for (var key in obj) {
+        cp[key] = obj[key];
+    }
+    return cp;
+}
+
 function rtcCall(localStream) {
     // local peer connection setup.
     var pcLocal = new window.webkitRTCPeerConnection(null);
     pcLocal.onicecandidate = function(e) {
-        e.candidate && console.log("pcLocal candidate: " + e.candidate.candidate);
+        e.candidate && console.log("[pcLocal.onicecandidate] " + e.candidate.candidate);
         e.candidate && pcRemote.addIceCandidate(new window.RTCIceCandidate(e.candidate));
     };
 
     // remote peer connection setup.
     var pcRemote = new window.webkitRTCPeerConnection(null);
     pcRemote.onicecandidate = function(e) {
-        e.candidate && console.log("pcRemote candidate: " + e.candidate.candidate);
+        e.candidate && console.log("[pcRemote.onicecandidate] " + e.candidate.candidate);
         e.candidate && pcLocal.addIceCandidate(new window.RTCIceCandidate(e.candidate));
     };
 
     pcRemote.onaddstream = function(e) {
         var rv = document.getElementById("remote");
         rv.src = window.URL.createObjectURL(e.stream);
-        console.log("remote stream " + rv.src);
+        console.log("[pcRemote.onaddstream] rv.src=remoteStream " + rv.src);
     };
 
     // trigger connection between local and remote peer.
     pcLocal.addStream(localStream);
-    pcLocal.createOffer(function(desc){
-        pcLocal.setLocalDescription(desc); // trigger pcLocal.onicecandidate().
-        pcRemote.setRemoteDescription(desc);
-        console.log("Offer from pcLocal: " + desc.sdp.length + " size sdp");
+    console.log("[pcLocal.addStream] add localStream to peer connection");
 
-        fireRemote(desc);
+    pcLocal.createOffer(function(offer){
+        pcLocal.setLocalDescription(offer); // trigger pcLocal.onicecandidate().
+        console.log("[pcLocal.createOffer] Request with offer " + offer.sdp.length + "B sdp as bellow:");
+        console.log(offer);
+
+        transmitOffer(offer);
     }, function(error){
         console.error(error);
     });
 
-    // fire remote when got local peer event.
-    var fireRemote = function(desc){
+    // Local transmit offer to remote.
+    var transmitOffer = function(offer) {
+        offer = copyObject(offer); // To demonstrate offer transmit over network in JSON.
+        setTimeout(onRemoteGotOffer, 200, offer);
+    };
+    var onRemoteGotOffer = function(offer) {
+        pcRemote.setRemoteDescription(offer);
+        console.log("[onRemoteGotOffer] Got offer " + offer.sdp.length + "B sdp as bellow:");
+        console.log(offer);
+
         // Since the 'remote' side has no media stream we need
         // to pass in the right constraints in order for it to
         // accept the incoming offer of audio and video.
-        pcRemote.createAnswer(function(desc){
-            pcRemote.setLocalDescription(desc); // trigger pcRemote.onicecandiate().
-            pcLocal.setRemoteDescription(desc);
-            console.log("Answer from pcRemote: " + desc.sdp.length + " size sdp");
+        pcRemote.createAnswer(function(answer){
+            pcRemote.setLocalDescription(answer); // trigger pcRemote.onicecandiate().
+            console.log("[pcRemote.createAnswer] Response offer=" + offer.sdp.length + "B answer " + answer.sdp.length + "B sdp as bellow:");
+            console.log(answer);
+
+            transmitAnswer(answer);
         }, function(error){
             console.error(error);
         });
+    };
+
+    // Remote transmit answer to local.
+    var transmitAnswer = function(answer) {
+        answer = copyObject(answer); // To demonstrate answer transmit over network in JSON.
+        setTimeout(onLocalGotAnswer, 200, answer);
+    };
+    var onLocalGotAnswer = function(answer) {
+        pcLocal.setRemoteDescription(answer);
+        console.log("[pcRemote.createAnswer] Got answer " + answer.sdp.length + "B sdp as bellow:");
+        console.log(answer);
     };
 }
