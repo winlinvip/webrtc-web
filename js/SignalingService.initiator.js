25e0be9ae7a7f2776e4ccca400fb2b92539e4250
winlin <winlin@vip.126.com>
2017-07-26 18:32:15 +0800

    Refine initiator with promise

--- js/SignalingService.initiator.js
+++ js/SignalingService.initiator.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // https://codelabs.developers.google.com/codelabs/webrtc-web/#6
-var lv = document.getElementById("local");
 var api = "";
 
 $("#start").click(function(){
@@ -10,89 +9,105 @@ $("#start").click(function(){
         api = "http://" + napi;
     }
 
-    navigator.webkitGetUserMedia({
-        video:true,audio:false
-    },function(stream){
+    new Promise(function(resolve, reject){
+        navigator.webkitGetUserMedia({
+            video:true,audio:false
+        },function(stream){
+            resolve(stream);
+        }, function(error){
+            reject(error);
+        });
+    }).then(function(stream){
+        var lv = document.getElementById("local");
         lv.src = window.URL.createObjectURL(stream);
-        console.log("[navigator.webkitGetUserMedia] lv.src=localStream " + lv.src);
+        console.log("[navigator.webkitGetUserMedia] lv.src= " + lv.src);
 
-        shareLocalStream(stream);
-    }, function(error){
-        console.error(error);
+        // Use a peer connection to share stream to responder.
+        var conn = new window.webkitRTCPeerConnection(null);
+        conn.addStream(stream);
+        console.log("[conn.addStream] add stream to peer connection");
+        return conn;
+    }).then(function(conn){
+        Promise.all([new Promise(function(resolve, reject){
+            conn.onicecandidate = function(e) {
+                if (!e.candidate) {
+                    return;
+                }
+
+                console.log("[conn.onicecandidate] " + e.candidate.candidate);
+
+                // Transmit initiator candidates info to signaling server.
+                var data = JSON.stringify(escapeCandicate(e.candidate.candidate));
+                $.ajax({
+                    type:"POST", async:true, url:api+"/api/webrtc/icandidates", contentType:"application/json", data:data,
+                    success: function(){ resolve(e.candidate.candidate); }, error: function(xhr,err) { reject(err); }
+                });
+            };
+        }), new Promise(function(resolve, reject){
+            conn.createOffer(function(offer){
+                conn.setLocalDescription(offer); // trigger conn.onicecandidate().
+                console.log("[conn.createOffer] Request with offer " + offer.sdp.length + "B sdp as bellow:");
+                console.log(offer);
+
+                // Create a offer so that the responder can answer.
+                var data = JSON.stringify(escapeOffer(offer));
+                $.ajax({
+                    type:"POST", async:true, url:api+"/api/webrtc/offer", contentType:"application/json", data:data,
+                    success: function(){ resolve(offer); }, error: function(xhr,err) { reject(err); }
+                });
+            }, function(error){
+                reject(error);
+            });
+        })]).then(function([candidate, offer]){
+            waitResponder(conn);
+        });
     });
 });
 
-function shareLocalStream(localStream) {
-    // Use a peer connection to share stream to responder.
-    var pcLocal = new window.webkitRTCPeerConnection(null);
-
-    // Transmit initiator candidates info to signaling server.
-    pcLocal.onicecandidate = function(e) {
-        if (!e.candidate) {
-            return;
-        }
-        console.log("[pcLocal.onicecandidate] " + e.candidate.candidate);
-        transmitInitiatorCandidate(e.candidate);
-    };
-    var transmitInitiatorCandidate = function(candidate) {
-        candidate = JSON.stringify(escapeCandicate(candidate));
-        $.ajax({type:"POST", async:false, url:api+"/api/webrtc/icandidates", contentType:"application/json", data:candidate});
-    };
-
-    // Add the stream to shared peer connection.
-    pcLocal.addStream(localStream);
-    console.log("[pcLocal.addStream] add localStream to peer connection");
-
-    // Create a offer so that the responder can answer.
-    pcLocal.createOffer(function(offer){
-        pcLocal.setLocalDescription(offer); // trigger pcLocal.onicecandidate().
-        console.log("[pcLocal.createOffer] Request with offer " + offer.sdp.length + "B sdp as bellow:");
-        console.log(offer);
-
-        transmitOffer(offer);
-    }, function(error){
-        console.error(error);
-    });
-
-    // Transmit initiator offer to signaling server.
-    var transmitOffer = function(offer) {
-        offer = JSON.stringify(escapeOffer(offer));
-        $.ajax({type:"POST", async:false, url:api+"/api/webrtc/offer", contentType:"application/json", data:offer});
-    };
-
-    // Wait for responder to reply the answer.
-    var onLocalGotAnswer = function(answer) {
+function waitResponder(pcLocal) {
+    new Promise(function(resolve, reject){
+        var waitAnswer = function(){
+            $.ajax({
+                type:"GET", async:true, url:api+"/api/webrtc/answer", contentType:"application/json",
+                success:function(data){
+                    var answer = unescapeOffer(JSON.parse(JSON.parse(data)[0]));
+                    resolve(answer);
+                }, error:function(){
+                    console.log("[waitAnswer] No answer, wait for a while.");
+                    setTimeout(waitAnswer, 1000);
+                }
+            });
+        };
+        setTimeout(waitAnswer, 0);
+    }).then(function(answer){
+        // Wait for responder to reply the answer.
         pcLocal.setRemoteDescription(answer);
         console.log("[onLocalGotAnswer] Got answer " + answer.sdp.length + "B sdp as bellow:");
         console.log(answer);
 
-        requestCandidates();
-    };
-    var waitAnswer = function(){
-        $.ajax({type:"GET", async:false, url:api+"/api/webrtc/answer", contentType:"application/json", success:function(data){
-            var answer = unescapeOffer(JSON.parse(JSON.parse(data)[0]));
-            onLocalGotAnswer(answer);
-        }, error:function(){
-            console.log("[waitAnswer] No answer, wait for a while.");
-            setTimeout(waitAnswer, 1000);
-        }});
-    };
-    setTimeout(waitAnswer, 0);
-
-    // When got answer from responder, request its candidates.
-    var requestCandidates = function() {
-        $.ajax({type:"GET", async:false, url:api+"/api/webrtc/rcandidates", contentType:"application/json", success:function(data){
-            data = JSON.parse(data) || [];
+        new Promise(function(resolve, reject){
+            // When got answer from responder, request its candidates.
+            var requestCandidates = function() {
+                $.ajax({
+                    type:"GET", async:true, url:api+"/api/webrtc/rcandidates", contentType:"application/json",
+                    success:function(data){
+                        data = JSON.parse(data) || [];
+                        resolve(data);
+                    }, error:function(){
+                        console.log("[requestCandidates] No responder candidates, wait for a while.");
+                        setTimeout(requestCandidates, 1000);
+                    }
+                });
+            };
+            requestCandidates();
+        }).then(function(data){
             for (var i = 0; i < data.length; i++) {
                 var candidate = unescapeCandicate(JSON.parse(data[i]));
                 pcLocal.addIceCandidate(new window.RTCIceCandidate(candidate));
                 console.log("[requestCandidates] Got responder candidate " + JSON.stringify(candidate));
             }
-        }, error:function(){
-            console.log("[requestCandidates] No responder candidates, wait for a while.");
-            setTimeout(requestCandidates, 1000);
-        }});
-    };
+        });
+    });
 }
 
 function escapeOffer(offer) {
